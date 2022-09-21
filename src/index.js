36b9b044fc8338650ad2b35aeb2bc3e9e4d72018
jellie <talentforest0501@gmail.com>
2022-09-21 21:04:23 +0900

    Feat: 3. 장바구니 토글 기능

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@
 import getProductData from './api/getProductData.js';
 import ProductList from './component/ProductList.js';
 
+// 1. 데이터 모킹하기
 const $productCardGrid = document.getElementById('product-card-grid');
+const fetchProductData = async () => {
+  const result = await getProductData();
+  productList.setState(result); //2. 상품 목록 렌더링하기
+};
+fetchProductData();
 
 const productList = new ProductList($productCardGrid, []);
 
-const fetchProductData = async () => {
-  const result = await getProductData();
-  productList.setState(result);
+// 3. 장바구니 토글 기능
+const $backdrop = document.getElementById('backdrop');
+const $shoppingCart = document.getElementById('shopping-cart');
+const $openCartBtn = document.getElementById('open-cart-btn');
+const $closeCartBtn = document.getElementById('close-cart-btn');
+
+const toggleShoppingCart = () => {
+  $backdrop.hidden = !$backdrop.hidden;
+  $shoppingCart.classList.toggle('translate-x-0');
+  $shoppingCart.classList.toggle('translate-x-full');
 };
 
-fetchProductData();
+$openCartBtn.addEventListener('click', toggleShoppingCart);
+$closeCartBtn.addEventListener('click', toggleShoppingCart);
+$backdrop.addEventListener('click', toggleShoppingCart);
+$productCardGrid.addEventListener('click', toggleShoppingCart);
