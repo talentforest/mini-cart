254b3a8192a14f9157d23e6fd625b491f89bb92c
jellie <talentforest0501@gmail.com>
2022-09-21 19:28:54 +0900

    Feat: 2. 상품목록 렌더링하기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // 이 곳에 정답 코드를 작성해주세요.
 import getProductData from './api/getProductData.js';
+import ProductList from './component/ProductList.js';
+
+const $productCardGrid = document.getElementById('product-card-grid');
+
+const productList = new ProductList($productCardGrid, []);
 
 const fetchProductData = async () => {
   const result = await getProductData();
+  productList.setState(result);
 };
 
 fetchProductData();
